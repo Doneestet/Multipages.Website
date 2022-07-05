95c313ceb0356e30874b9e2b6016928776a7c1eb
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-07-05 16:44:16 +0300

    created and confique all sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/sliders/slider-main.js';
+import MiniSlider from './modules/sliders/slider-mini.js';
 import VideoPlayer from './modules/playVideo.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({
+        btns: '.next',
+        container: '.page'
+    });
     slider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        prev: '.showup__prev',
+        next: '.showup__next'
+    });
+    showUpSlider.init();
+
+    const moduleSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        prev: '.modules__info-btns .slick-prev',
+        next: '.modules__info-btns .slick-next'
+    });
+    moduleSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        prev: '.feed__slider .slick-prev',
+        next: '.feed__slider .slick-next'
+    });
+    feedSlider.init();
 
     const player = new VideoPlayer('.showup .play', '.overlay' );
     player.init();
